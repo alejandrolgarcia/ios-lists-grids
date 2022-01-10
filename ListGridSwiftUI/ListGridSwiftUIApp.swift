9d582cb6bf8d1c80a20f57a66052f4c1e967b94f
Luis Garcia <lalejandro.1988@gmail.com>
2022-01-09 21:36:15 -0800

    Initial Commit

---
+++ ListGridSwiftUI/ListGridSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  ListGridSwiftUIApp.swift
+//  ListGridSwiftUI
+//
+//  Created by Luis Garcia on 9/01/22.
+//
+
+import SwiftUI
+
+@main
+struct ListGridSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
